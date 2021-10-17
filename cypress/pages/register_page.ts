85eb293127f27d99ebb02fc5eab793e890cc69da
Snehal Bhuktar <bhuktarsnehal7@gmail.com>
2021-10-17 14:33:40 +0200

    chore: check without parallel

--- cypress/pages/register_page.ts
+++ cypress/pages/register_page.ts
@@ -19,9 +19,9 @@ export default class RegisterPage{
             .and('not.be.empty')
             .then(cy.wrap)
             .find('#recaptcha-anchor')
-            // .should('be.visible')
+            .should('be.visible')
             .click({force: true});
-        // cy.get(this.checkBox).click();
+        // cy.get(this.checkBox).click({force: true});
         cy.get(this.registerButton).click();
     }
 
